b8b878d37e3921bbd11e779f0b3ecf1d1675bd43
leosantosw <leonardosantosx0077@gmail.com>
2023-04-21 20:43:23 -0300

    implement answer model

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,3 +1,11 @@
-class Intructor {
-    
+import { randomUUID } from "node:crypto"
+
+export class Instructor {
+    public id: string
+    public name: string
+
+    constructor(name: string, id?: string) {
+        this.name = name
+        this.id = id ?? randomUUID()
+    }
 }
