adedacb8ff6d26a9bbcd9d60ada12b390f55f207
leosantosw <leonardosantosx0077@gmail.com>
2023-04-22 22:09:24 -0300

    test: refactor tests

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from 'vitest'
+import { Answer } from '../entities/answer'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answer-repository'
 
-test('should be create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: AnswerRepository = {
+    create: async (answer: Answer) => { 
+        return
+    }
+}
 
-    const answer = answerQuestion.execute({
+test('should be create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+    const answer = await answerQuestion.execute({
         questionId: '1',
         instructorId: '1',
         content: 'This is a new answer'
