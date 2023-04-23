d7d1750f4d224a2bfef4434dcff28812ffae5b1a
leosantosw <leonardosantosx0077@gmail.com>
2023-04-23 00:10:24 -0300

    test: create test for slug class

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,11 @@
+import { Slug } from './slug';
+import { expect, test } from 'vitest';
+
+test('should be able to create a slug from text', () => {
+  const inputText = "Hello, this is an example of text to create a slug."
+  const expectedSlug = "hello-this-is-an-example-of-text-to-create-a-slug"
+
+  const actualSlug = Slug.createSlug(inputText)
+  
+  expect(actualSlug).toEqual(expectedSlug)
+})
