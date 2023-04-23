74a9813388a887bd27ea9e203548e985dbf868b4
leosantosw <leonardosantosx0077@gmail.com>
2023-04-23 01:04:54 -0300

    refactor: implement class unique entity id

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,13 @@
 import { Slug } from './value-objects/slug'
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
     title: string
     slug: Slug
     content: string
-    studentId: string 
-    authorId: string
+    studentId: UniqueEntityID 
+    authorId: UniqueEntityID
 }
 
 export class Question extends Entity<QuestionProps> {}
